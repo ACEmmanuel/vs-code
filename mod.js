9b76a54ea0c2def3218af35e2f7352fe4f866491
ACEmmanuel <ahanotuemmanuel7@gmail.com>
2024-01-15 02:22:18 -0800

    praticing event loop

--- mod.js
+++ mod.js
@@ -5,6 +5,11 @@ const server = http.createServer((req, res) => {
     res.end('Hello World');
 })
 
-server.listen(5000, ()=>{
-    console.log('Server listening to port 5000.........')
-})
+// server.listen(5000, ()=>{
+    // console.log('Server listening to port 5000.........')
+// })
+
+
+for (let i = 0; i < 10; i++) {
+    
+}
